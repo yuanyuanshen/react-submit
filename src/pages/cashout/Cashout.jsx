7a66c5ce5f49ff6001c471bcabe279a0950c8914
shenyuanyuan10 <shenyuanyuan@jd.com>
2019-09-04 21:24:46 +0800

    添加提现页面

--- src/pages/cashout/Cashout.jsx
+++ src/pages/cashout/Cashout.jsx
@@ -1,34 +1,53 @@
 import React , {Component} from 'react';
-import { InputNumber,Button } from 'antd';
+import { InputNumber,Button,message } from 'antd';
 import Header from '@/components/header/header'
 import './Cashout.css'
 
-function onChange(value) {
-  console.log('changed', value);
-}
+class CashOut extends Component {
+  state = {
+    money: 0,
+    usefulMoney: 800
+  };
 
-export default class CashOut extends Component {
+  onChange = value => {
+    this.setState({money: value});
+  };
+
+  handleCashout = (e) => {
+    let value = e.target.value;
+    if(this.state.money <= 0){
+      message.error('请输入提现金额')
+    }else{
+      message.success('提现成功')
+      let sub = this.state.usefulMoney - this.state.money;
+      this.setState({
+        usefulMoney: sub
+      })
+      value = value.replace(/^0+/,'0');
+    }
+  };
+
   render(){
     return (
       <div>
         <Header title="提现"></Header>
         <div>
-          <div className="remainder">您当前的可提现金额为：￥800</div>
+          <div className="remainder">您当前的可提现金额为：￥{this.state.usefulMoney}</div>
           <div className="cashout">
             <div className="to-cashout">请输入提现金额（元）</div>
             <InputNumber 
-              min={1} max={10} 
-              defaultValue={3} 
-              onChange={onChange} 
+              min={1} 
+              max={10} 
+              onChange={this.onChange} 
               className="input-cashout"       
               defaultValue={0}
-              min={0}
-              max={100}
               formatter={value => `￥ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}/>
           </div>
-          <Button type="primary" size="large" className="cashout-btn">申请提现</Button>
+          <Button type="primary" size="large" className="cashout-btn" onClick={this.handleCashout}>申请提现</Button>
         </div>
       </div>
     )
   }
 }
+
+export default CashOut;
