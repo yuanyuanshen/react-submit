19b08f5710cdaaacfa2f33fa7a73a31917c9be63
shenyuanyuan10 <shenyuanyuan@jd.com>
2019-09-05 11:18:50 +0800

    申请提现页面

---
+++ src/components/dialog/dialog.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Modal } from 'antd';
+
+class Dialog extends React.Component {
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = () => {
+    this.props.transferVisible(false)
+  };
+
+  render() {
+    return (
+      <div>
+        <Modal
+          title={this.props.title}
+          visible={this.props.visible}
+          okText="确认"
+          cancelText="取消"
+          onOk={this.handleCancel}
+          onCancel={this.handleCancel}
+        >
+          <p>{this.props.des}</p>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default Dialog;
--- src/pages/cashout/Cashout.jsx
+++ src/pages/cashout/Cashout.jsx
@@ -2,31 +2,40 @@ import React , {Component} from 'react';
 import { InputNumber,Button,message } from 'antd';
 import Header from '@/components/header/header'
 import './Cashout.css'
+import Dialog from '@/components/dialog/dialog'
 
 class CashOut extends Component {
   state = {
     money: 0,
-    usefulMoney: 800
+    usefulMoney: 800,
+    visible: false
   };
 
-  onChange = value => {
+  onChange = (value) => {
     this.setState({money: value});
   };
 
+  transferVisible(visible) {
+    this.setState({
+      visible
+    });
+  }
+
   handleCashout = (e) => {
-    let value = e.target.value;
+    console.log(this.state.money)
     if(this.state.money <= 0){
       message.error('请输入提现金额')
     }else{
-      message.success('提现成功')
+      // message.success('提现成功')
       let sub = this.state.usefulMoney - this.state.money;
       this.setState({
-        usefulMoney: sub
+        usefulMoney: sub,
+        money: 0,
+        visible: true
       })
-      value = value.replace(/^0+/,'0');
     }
   };
-
+  
   render(){
     return (
       <div>
@@ -36,15 +45,17 @@ class CashOut extends Component {
           <div className="cashout">
             <div className="to-cashout">请输入提现金额（元）</div>
             <InputNumber 
-              min={1} 
-              max={10} 
+              min={0} 
+              max={200} 
               onChange={this.onChange} 
               className="input-cashout"       
               defaultValue={0}
+              value={this.state.money}
               formatter={value => `￥ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}/>
           </div>
           <Button type="primary" size="large" className="cashout-btn" onClick={this.handleCashout}>申请提现</Button>
         </div>
+        <Dialog title="提现成功" des="您以提现成功，可到提现记录中查看" visible={this.state.visible} transferVisible={(visible)=>this.transferVisible(visible)}></Dialog>
       </div>
     )
   }
